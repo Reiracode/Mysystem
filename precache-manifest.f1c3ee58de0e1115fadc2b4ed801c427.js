ac95bb2a288e1b54aa06f88d6f5b2cec9333a9c6
Reiracode <reira.igg@gmail.com>
2022-08-25 11:37:20 +0800

    Updates

---
+++ precache-manifest.f1c3ee58de0e1115fadc2b4ed801c427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd62816eaff95216e651f0651a5769a2",
+    "url": "/Mysystem/index.html"
+  },
+  {
+    "revision": "bdc3ef7cd1229e8bb744",
+    "url": "/Mysystem/static/css/2.700f80b7.chunk.css"
+  },
+  {
+    "revision": "c86cc5a47295adb71ea1",
+    "url": "/Mysystem/static/css/main.33c72aff.chunk.css"
+  },
+  {
+    "revision": "bdc3ef7cd1229e8bb744",
+    "url": "/Mysystem/static/js/2.bf3a0bf8.chunk.js"
+  },
+  {
+    "revision": "c86cc5a47295adb71ea1",
+    "url": "/Mysystem/static/js/main.4b2b332c.chunk.js"
+  },
+  {
+    "revision": "ba1afb0da3e37b8a3594",
+    "url": "/Mysystem/static/js/runtime~main.8f896d5a.js"
+  }
+]);
