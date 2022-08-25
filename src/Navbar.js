a50723589e557834de4dfdaff57718c2bcfd7b85
Reiracode <reira.igg@gmail.com>
2022-08-25 11:35:30 +0800

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,48 @@
+import React  from "react";
+import { NavLink,  useLocation } from "react-router-dom";
+import { logOut, useAuthDispatch } from "./Context";
+ 
+const Navbar = (props) => {
+  const location = useLocation();
+  console.log(location.pathname);
+  // logout
+  const dispatch = useAuthDispatch();
+  
+  const handleLogout = async () => {
+    try {
+      let response = await logOut(dispatch);
+      console.log(response);
+      props.history.push("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return location.pathname !== "/login" ? (
+    <nav className="main-menu">
+      <ul>
+        <li>
+          <NavLink to="/prform">PrForm</NavLink>
+        </li>
+        <li>
+          <NavLink to="/poform">PoForm</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/inbox">收件匣</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">寄件匣</NavLink>
+        </li>
+
+        <li>
+          <a onClick={handleLogout}>LOGOUT</a>
+        </li>
+      </ul>
+    </nav>
+  ) : null;
+};
+
+export default Navbar;
